ef03f8394d8e7f6c2e854e1bd32b78b1f7c16a01
tejeshbhalla <tejeshbhalla1@gmail.com>
2026-02-09 15:44:49 +0530

    set-seed

--- train.py
+++ train.py
@@ -4,6 +4,10 @@ import json
 import os 
 
 
+torch.manual_seed(3407) 
+torch.cuda.manual_seed_all(3407) 
+
+
 max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
 dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
 load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
